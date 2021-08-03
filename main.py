b95488f17f8e671000ff6b8ce86d7a8a0bdcbf04
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-03 16:55:24 +0900

    feat: add get_addrs
    email list_fastcacmpus 파일에서 email_list에 보낼 이메일 주소를 확인후
    리스트에 담아서 반환합니다.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from NaverNewsCrawler import NaverNewsCrawler
+from openpyxl import load_workbook
 
 
 def main():
@@ -6,6 +7,21 @@ def main():
     crawler = NaverNewsCrawler(keyword)
     file_name = input('저장할 파일명을 입력하세요: ')
     crawler.get_news(file_name)
+
+
+def get_addrs():
+    wb = load_workbook('email list_fastcampus news.xlsx')
+    data = wb.active
+    area = data['B3:C4']
+
+    addrs = []
+    for row in area:
+        addr = []
+        for cell in row:
+            addr.append(cell.value)
+        addrs.append(addr)
+
+    return addrs
 
 
 if __name__ == '__main__':
