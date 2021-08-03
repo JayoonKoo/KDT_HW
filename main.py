249900cebc0c3d2cc6ab32d0a1c072069f9cd43c
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-03 17:11:13 +0900

    feat: add send_mail code
    sending_email 에서 불러온 send_mail를 사용해서 이메일을 보냅니다.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from NaverNewsCrawler import NaverNewsCrawler
 from openpyxl import load_workbook
+from sending_email import send_mail
+import os
 
 
 def main():
     keyword = input('원하는 키워드를 입력하세요: ')
     crawler = NaverNewsCrawler(keyword)
-    file_name = input('저장할 파일명을 입력하세요: ')
+    file_name = input('저장할 파일명을 입력하세요(현재 경로 아니라면 경로 포함): ')
     crawler.get_news(file_name)
+
+    # 이메일 확인
+    addrs = get_addrs()
+    contents = '안녕하세요. 네이버에서 기사 크롤링 한거 엑셀 파일로 보내드립니다.'
+    for addr in addrs:
+        name, email = addr
+        send_mail(name, email, '기사 크롤링입니다.', contents, file_name)
 
 
 def get_addrs():
@@ -22,6 +31,9 @@ def get_addrs():
         addrs.append(addr)
 
     return addrs
+
+
+def validate_excel(file_name):
 
 
 if __name__ == '__main__':
