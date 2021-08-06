43e3339637655d6150f4764352b8612c8b1434e7
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-06 15:17:34 +0900

    refactor: getItemByAmount
    주석 추가
    let -> const

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ const data = [
 
 // 사용자 입력 받기
 const line = prompt('최대 금액을 입력해주세요.');
-const amount = +line;
+const amount = Number(line);
 
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
-const item = getItemByAmount(data, amount);
+const item = getItemByAmount(amount);
 
 const msg = item ? 
     `${amount}원으로 살 수 있는 가장 비싼 상품은 [${item.name}]이고, 가격은 ${item.price}원입니다.` : 
@@ -26,12 +26,20 @@ const msg = item ?
 // 결과 출력
 alert(msg);
 
+/**
+ * 사용자가 입력한 값과 음식 정보를 가져와서 살수 있는 가장 비싼 음식 구하기 
+ * 사용자가 입력한 값으로 살수 있는 아이템이 없으면 null
+ * @function getItemByAmount
+ * @param {array} data 음식 정보들
+ * @param {number} amount 사용자의 가격 
+ * @returns {object||null} possibleDish.price? possibleDish: nulll
+ */
 // data, amount 를 받아서 살수 있는 가장 비싼 음식인 possibleDish 를 반환함. 
-function getItemByAmount(data, amount) {
+function getItemByAmount(amount) {
 	// 유혀성 검사 amount가 글자면 NaN 이 되는것, 0이하 검사 
 	if (amount <= 0 || !(amount)) return null;
 	
-	let possibleDish = {
+	const possibleDish = {
 		name: '',
 		price: 0,
 	};
